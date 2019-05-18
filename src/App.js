e5157faf13c8827f087cdf3061157fcadda46567
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-18 12:27:36 +0300

    [#166102507] Sets up react router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignIn from './containers/auth/SignIn';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path='/signin' component={SignIn} />
+        </Switch>
+      </div>    
+    </BrowserRouter>
+  );
+}
+
+export default App;
